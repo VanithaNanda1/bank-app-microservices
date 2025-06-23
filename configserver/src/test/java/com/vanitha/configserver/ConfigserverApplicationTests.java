51bdeeb7d25600d057483800b84ce0783aac1e98
VanithaNanda1 <149882585+VanithaNanda1@users.noreply.github.com>
2025-06-23 09:14:10 +0530

    Commit for Bank app with 3 microservices , config server , spring cloud functions with kafka
    Spring boot application with microservices , config server , spring cloud functions with kafka

---
+++ configserver/src/test/java/com/vanitha/configserver/ConfigserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vanitha.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigserverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
